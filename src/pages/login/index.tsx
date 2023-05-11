eaea43c51fbc6195606a0e6cf420ce9411622be8
Maruf Bepary <bepary71@gmail.com>
2023-05-11 22:41:26 +0100

    Added documentation for the log in page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,11 +3,19 @@ import { useUser, useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useRouter } from "next/router";
 import { Auth, ThemeSupa } from "@supabase/auth-ui-react";
 
+/**
+ * Displays the login page by default.
+ * Links for sign up and forgot password are also available.
+ * If there is a user logged in, redirect to the main feed.
+ * This is fully handled by Supabase.
+ * @returns {JSX.Element}: The login page
+ */
 const Login: NextPage = () => {
   const supabaseClient = useSupabaseClient();
   const user = useUser();
   const router = useRouter();
 
+  // If there is a user logged in, redirect to the main feed
   if (user) {
     router.push("/mainFeed");
   }
