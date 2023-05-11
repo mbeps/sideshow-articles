95ad3076780edff1afca14f46712b054b1a9391c
Maruf Bepary <bepary71@gmail.com>
2023-05-11 22:37:24 +0100

    Added documentation for the app structure

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,21 @@
 import React from "react";
-import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
 import { CssBaseline } from "@nextui-org/react";
 
+/**
+ * Modifies the initial HTML document structure and inject custom logic by extending the base Document class.
+ * In this particular case, it retrieves the initial props and modifies the styles array.
+ * The render method defines the structure of the HTML document,
+ * including the language, head section, main content, and necessary Next.js scripts.
+ */
 class MyDocument extends Document {
-  static async getInitialProps(ctx : DocumentContext) {
+  static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     return {
       ...initialProps,
